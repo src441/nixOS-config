6de833879b05c03af94615042b55fb5817f68d85
src441 <cris441git@gmail.com>
2026-08-22 18:54:21 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
-  # extra nix features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Use nvidia instead of nouveau.
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -26,9 +21,8 @@
     package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
-  # Kernel parameters
   boot.kernelParams = [ "nvidia-drm.fbdev=1" ];
-  # XDG config 
+
   xdg.portal = {
   enable = true;
   extraPortals = [ 
@@ -46,7 +40,6 @@
   XDG_DATA_DIRS = ["$HOME/.nix-profile/share"];
 };
 
-  # Remove pre-installed KDE apps.
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     qrca
     gwenview
@@ -55,27 +48,16 @@
     kwalletmanager
   ];
   
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Lisbon";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "pt_PT.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -90,30 +72,22 @@
     LC_TIME = "pt_PT.UTF-8";
   };
 
-  # Enable flatpak
   services.flatpak.enable = true;
  
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = false;
 
-  # Desktops and WMs
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "pt";
     variant = "nodeadkeys";
   };
 
-  # Configure console keymap
   console.keyMap = "pt-latin1";
 
-  # Enable CUPS to print documents.
   services.printing.enable = false;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -121,18 +95,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."cris441" = {
     isNormalUser = true;
     description = "cris441";
@@ -146,14 +110,10 @@
     ]; 
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     fish
     fastfetch
@@ -163,31 +123,5 @@
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "26.05"; # Did you read the comment?
-
 }
